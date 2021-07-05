a281b121ba6d2ed89a3a0b038066e92907c5fc11
Leonardo Zabala <leosoft3031@gmail.com>
2021-07-05 11:54:38 -0300

    Version 1.1

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,12 +9,19 @@ export class GifsService {
 
   //Crear Get
   get historial(){
+    
     return [...this._historial];
   }
 
-  buscarGifs(query:string){
-    this._historial.unshift(query);
+  buscarGifs(query:string = ''){
     
+    query = query.toLocaleLowerCase();
+   
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+    }
+
+    this._historial = this._historial.splice(0,10);
     console.log(this._historial);
   }
 }
